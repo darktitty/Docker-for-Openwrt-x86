67f4ffa56aacae88a07cafe7144e011e2223c7c5
darktitty <42826254+darktitty@users.noreply.github.com>
2019-06-28 12:49:41 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,15 @@
 cd /mnt/sdb1
 wget https://download.docker.com/linux/static/stable/x86_64/docker-18.09.7.tgz
 tar -xzvf docker-18.09.7.tgz
-cd ./docker
+cd docker
 mkdir bin
-mv * ./bin
+mv * bin
+mkdir data
+mkdir config
+cd config
+wget https://github.com/darktitty/Docker-for-Openwrt-x86/blob/master/daemon.json
 ln -s /mnt/sdb1/docker/bin/* /usr/sbin
 cd /etc/init.d
-
-
-/etc/init.d/docker start
+wget https://github.com/darktitty/Docker-for-Openwrt-x86/blob/master/docker
+chmod +x docker
+./docker start
